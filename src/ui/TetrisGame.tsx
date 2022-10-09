75e2ee5faad2bc034c63cba764c98cf0b5299903
Dave Cluderay <daveclud@hotmail.com>
2022-10-09 20:46:59 +0100

    More UI experimentation.

--- src/ui/TetrisGame.tsx
+++ src/ui/TetrisGame.tsx
@@ -3,7 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { PlayArea } from './PlayArea';
 import { Brick } from './Brick';
 import { ReactNode, useMemo } from 'react';
-import { MagentaTetromino, produceRandomTetromino } from '../game/Tetrominoes';
+import { CyanTetromino, GreenTetromino, MagentaTetromino, produceRandomTetromino } from '../game/Tetrominoes';
 import { Tetromino } from './Tetromino';
 import { BrickColour } from '../game/SharedTypes';
 
@@ -14,22 +14,22 @@ type TetrisGameProps = {
 function TetrisGame(props: TetrisGameProps) {
     const bricks = useMemo(() => {
         const results: ReactNode[] = [];
-        for (var y = 0; y < 20; y++) {
+        for (var y = 0; y < 8; y++) {
             for (var x = 0; x < 10; x++) {
-                if (Math.random() * y < 2) {
+                if (Math.random() * y < 1) {
                     results.push(<Brick key={`${x}-${y}`} position={[x, y, 0]} colour={produceRandomTetromino().colour} />);
                 }
             }
         }
         return results;
-    }, [])
+    }, []);
     return (
         <Canvas className="TetrisGame" orthographic camera={{ zoom: 35, position: [0, 15, 30], far: 100, near: 20 }} onCreated={state => state.camera.lookAt(5, 10, 0)}>
             <ambientLight intensity={0.5} />
             <pointLight position={[0, -10, 10]} intensity={0.75} />
             <PlayArea position={[0, 0, -1]} width={10} height={20}>
                 {bricks}
-                <Tetromino colour={BrickColour.Magenta} layout={MagentaTetromino.layout} position={[4, 10, 1]} rotationZ={0} />
+                <Tetromino colour={BrickColour.Green} layout={GreenTetromino.layout} position={[4, 10, 0.001]} rotationZ={-0.5} />
             </PlayArea>
         </Canvas>
     )
--- src/ui/Tetromino.tsx
+++ src/ui/Tetromino.tsx
@@ -1,6 +1,7 @@
 import { BrickColour } from "../game/SharedTypes";
 import { animated, useSpring } from "@react-spring/three"
 import { useMemo } from "react";
+import { Brick } from "./Brick";
 
 type TetrominoProps = {
   position: [x: number, y: number, z: number],
@@ -10,16 +11,17 @@ type TetrominoProps = {
 };
 
 function Tetromino(props: TetrominoProps) {
-  const spring = useSpring({ position: props.position, rotationZ: props.rotationZ * Math.PI / 2 })
-  const material = <meshStandardMaterial color={props.colour} />
   const layout = props.layout;
+  const size = layout.length;
+  const adjustedPosition = adjustPosition(props.position, [size / 2, 1 - size / 2, 0]);
+  const spring = useSpring({ position: adjustedPosition, rotationZ: props.rotationZ * Math.PI / 2 })
 
   const brickPositions = useMemo<[x: number, y: number, z: number][]>(() => {
     const results: [x: number, y: number, z: number][] = [];
-    for (let y = 0; y < layout.length; y++) {
-      for (let x = 0; x < layout[y].length; x++) {
+    for (let y = 0; y < size; y++) {
+      for (let x = 0; x < size; x++) {
         if (layout[y][x] === 1) {
-          results.push([x, layout.length - y - 1, 0]);
+          results.push([x - size / 2, size / 2 - y - 1, 0]);
         }
       }
     }
@@ -28,12 +30,18 @@ function Tetromino(props: TetrominoProps) {
 
   return (
     <animated.group position={spring.position} rotation-z={spring.rotationZ}>
-      {brickPositions.map((p, i) => <mesh key={i} position={p}>
-        <boxGeometry args={[1, 1, 1]} />
-        {material}
-      </mesh>)}
+      {brickPositions.map((p, i) => <Brick key={i} position={p} colour={props.colour} />)}
     </animated.group>
   )
+}
+
+// Position provided is the bottom-left corner.
+function adjustPosition(position: [x: number, y: number, z: number], size: [x: number, y: number, z: number]) : [x: number, y: number, z: number] {
+    return [
+        position[0] + size[0],
+        position[1] + size[1],
+        position[2] + size[2]
+    ];
 }
 
 export { Tetromino }
